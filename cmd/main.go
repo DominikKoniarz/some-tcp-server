05abb045f71b3b245b14b2956bbc2a70a239bf1e
DominikKoniarz <dominik.koniarz.development@gmail.com>
2025-01-03 22:30:06 +0100

    Add basic server with request parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,67 +1,21 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"strconv"
+
+	server "github.com/DominikKoniarz/some-tcp-server/internal"
 )
-
-type ClientRequest struct {
-	command string
-	data    string
-}
-
-func ParseRequest(rawData []byte) {
-	// 4 bytes for length of whole command
-	// 4 bytes for protocol version
-	// 4 bytes for message type
-	// rest of the bytes for data
-
-	// 4 bytes for length of whole command
-	length := rawData[:4]
-	// 4 bytes for protocol version
-	protocolVersion := rawData[4:8]
-	// 4 bytes for message type
-	messageType := rawData[8:9]
-	// rest of the bytes for data
-	data := rawData[9:]
-
-	if string(protocolVersion) != "0001" {
-		fmt.Println("Invalid protocol version")
-		return
-	}
-
-	_, err := strconv.Atoi(string(length))
-	if err != nil {
-		fmt.Println("Error converting length to integer:", err)
-		return
-	}
-
-	stringMessageType := string(messageType)
-
-	if stringMessageType == "I" {
-		// split data by 0x00 to get username and password
-		parts := bytes.Split(data, []byte{0x00})
-		if len(parts) != 2 {
-			fmt.Println("Invalid data format")
-			return
-		}
-		username := string(parts[0])
-		password := string(parts[1])
-
-		fmt.Printf("Username: %s, Password: %s\n", username, password)
-	} else {
-		fmt.Println("Invalid message type")
-	}
-}
 
 func main() {
 	protocolVersion := "0001"
 	messageType := "I"
-	data := "user1\x00password1"
+	data := "db1\x00user1"
 	length := fmt.Sprintf("%04d", 4+4+1+len(data))
 
-	message := []byte(length + protocolVersion + messageType + data)
+	_ = []byte(length + protocolVersion + messageType + data)
 
-	ParseRequest(message)
+	server := server.NewServer()
+	server.Start()
+
+	defer server.Stop()
 }
---
+++ internal/server.go
@@ -0,0 +1,127 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+)
+
+type Server struct {
+	s      *net.Listener
+	logger *log.Logger
+}
+
+type ParsedRequest struct {
+	// length          int
+	protocolVersion string
+	messageType     string
+	data            string
+}
+
+func (pr ParsedRequest) Stringify() string {
+	return fmt.Sprintf("Protocol version: %s, Message type: %s, Data: %s", pr.protocolVersion, pr.messageType, pr.data)
+}
+
+func NewServer() *Server {
+	server, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+
+	return &Server{
+		s:      &server,
+		logger: log.New(log.Writer(), "Server: ", log.LstdFlags),
+	}
+}
+
+func (srv *Server) handleConnection(conn net.Conn) {
+	srv.logger.Println("New connection:", conn.RemoteAddr())
+
+	defer func() {
+		srv.logger.Println("Closing connection:", conn.RemoteAddr())
+		conn.Close()
+	}()
+
+	for {
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			srv.logger.Println("Error reading data:", err)
+			return
+		}
+		fmt.Println("Received data:", string(buf[:n]))
+		parsedRequest, err := srv.ParseRequest(buf[:n])
+		if err != nil {
+			srv.logger.Println("Error parsing request:", err)
+
+			if _, err := conn.Write([]byte(err.Error())); err != nil {
+				srv.logger.Println("Error writing response:", err)
+				return
+			}
+
+			continue
+		}
+
+		fmt.Println("Parsed request:", parsedRequest)
+
+		if _, err := conn.Write([]byte(parsedRequest.Stringify())); err != nil {
+			srv.logger.Println("Error writing response:", err)
+		}
+
+	}
+
+}
+
+func (srv *Server) Start() {
+	srv.logger.Println("Server started")
+
+	for {
+		conn, err := (*srv.s).Accept()
+		if err != nil {
+			srv.logger.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go srv.handleConnection(conn)
+	}
+}
+
+func (srv *Server) Stop() {
+	(*srv.s).Close()
+}
+
+func (srv *Server) ParseRequest(rawData []byte) (ParsedRequest, error) {
+	if len(rawData) < 5 {
+		return ParsedRequest{}, errors.New("invalid data format")
+	}
+
+	// 4 bytes for length of whole command
+	// length := rawData[:4]
+	// 4 bytes for protocol version
+	protocolVersion := rawData[:4]
+	// 4 bytes for message type
+	messageType := rawData[4:5]
+	// rest of the bytes for data
+	data := rawData[5:]
+
+	if string(protocolVersion) != "0001" {
+		return ParsedRequest{}, errors.New("invalid protocol version")
+	}
+
+	// intLength, err := strconv.Atoi(string(length))
+	// if err != nil {
+	// 	log.Println("Error converting length to integer:", err)
+	// 	return ParsedRequest{}, errors.New("error converting length to integer")
+	// }
+
+	stringMessageType := string(messageType)
+
+	return ParsedRequest{
+		// length:          intLength,
+		protocolVersion: string(protocolVersion),
+		messageType:     stringMessageType,
+		data:            string(data),
+	}, nil
+
+}
